946cf3132d1336b9a9fed17a3e075cbbc14df8da
florp <jens.krewald@gmail.com>
2022-05-31 01:39:12 +0200

    Mount flags can be specified on the command line.

--- lib/socket.h
+++
@@ -1,8 +0,0 @@
-
-#include <sys/types.h>
-
-int fusesock_transfer(int fd, int sock);
-int fusesock_bind(char *sockpath);
-int fusesock_run(int fd, char* sockpath);
-int do_socket(char *mnt, char* options, int flags, char* sockpath);
-
