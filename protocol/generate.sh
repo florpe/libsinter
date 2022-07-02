177b26a902c60a3d0a35181261425a80c2863d34
florp <jens.krewald@gmail.com>
2022-07-02 18:36:14 +0200

    Factored out struct definitions.

--- protocol/generate.sh
+++ protocol/generate.sh
@@ -2,9 +2,10 @@ set -euo pipefail
 
 for vdir in $(find . -maxdepth 1 -regex '\./v[0-9]+\.[0-9]+' -printf "%f" | sort) ; do
     jq --compact-output --slurp --arg vdir "$vdir" \
-        '{($vdir): {"headers": .[0], "opcodes": .[1], "operations": .[2]}}' \
+        '{($vdir): {"headers": .[0], "opcodes": .[1], "operations": .[2], "structs": .[3]}}' \
         "./$vdir/headers.json" \
         "./$vdir/opcodes.json" \
-        "./$vdir/operations.json"
+        "./$vdir/operations.json" \
+        "./$vdir/structs.json"
 done | jq --compact-output 'reduce . as $v ({}; . + $v)' > protocol.json
 
