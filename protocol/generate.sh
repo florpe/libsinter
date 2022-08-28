3423485e41aa786fca2cde97c0ba354f0557924f
florp <jens.krewald@gmail.com>
2022-08-28 18:15:52 +0200

    Protocol versioning now happens at the file level.

--- protocol/generate.sh
+++ protocol/generate.sh
@@ -2,10 +2,12 @@ set -euo pipefail
 
 for vdir in $(find . -maxdepth 1 -regex '\./v[0-9]+\.[0-9]+' -printf "%f" | sort) ; do
     jq --compact-output --slurp --arg vdir "$vdir" \
-        '{($vdir): {"headers": .[0], "opcodes": .[1], "operations": .[2], "structs": .[3]}}' \
+        '{"headers": .[0], "opcodes": .[1], "operations": .[2], "structs": .[3]}' \
         "./$vdir/headers.json" \
         "./$vdir/opcodes.json" \
         "./$vdir/operations.json" \
-        "./$vdir/structs.json"
-done | jq --compact-output 'reduce . as $v ({}; . + $v)' > protocol.json
+        "./$vdir/structs.json" \
+        > "protocol-$vdir.json"
+done
+cp "protocol-$vdir.json" protocol-latest.json
 
